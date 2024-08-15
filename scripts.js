6f37eac0ad6570974342392c819352b43e18a4cf
isaias2210 <155275538+isaias2210@users.noreply.github.com>
2024-08-15 15:22:57 -0500

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-// scripts.js
+// script.js
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('.main-nav');
+    const nav = document.querySelector('header nav');
 
-    menuToggle.addEventListener('click', () => {
+    menuToggle.addEventListener('click', function() {
         nav.classList.toggle('active');
+        const menuIcon = menuToggle.querySelector('.menu-icon');
+        menuIcon.classList.toggle('active');
     });
 });
